310f794a1d9c05ce47ad89d5ef244dfccf6a7f8f
AyakaB <ayaka.bando22@gmail.com>
2023-01-16 11:16:38 -0800

    solved game state #7

--- snake_lab/game_state/snake.js
+++ snake_lab/game_state/snake.js
@@ -1,7 +1,7 @@
 // - mutable object variables for both the snake & the apple, called "snake" & "apple"
 let snake = {
-  x: '100px',
-  y: '100px',
+  x: 100,
+  y: 100,
   // snake velocity. moves one grid length every frame in either the x or y direction
   dx: grid,
   dy: 0,
@@ -12,7 +12,7 @@ let snake = {
   // - a default value for initial snake length, called "maxCells", with an initial value of 1
   maxCells: 1,
 };
-let apple = { x: '50px', y: '50px' };
+let apple = { x: 50, y: 50 };
 
 // - an immutable variable for the canvas grid, called "grid"
 const grid = 16;
@@ -43,42 +43,68 @@ const loop = () => {
 
   // to clear the entire canvas
   context.clearRect(0, 0, canvas.width, canvas.height);
-};
 
-// snake movement
-snake.x += snake.dx;
-snake.y += snake.dy;
+  // snake movement
+  snake.x += snake.dx;
+  snake.y += snake.dy;
 
-// situation where the snake collides with the horizontal axises
-if (snake.x < 0) {
-  snake.x = canvas.width - grid;
-} else if (snake.x >= canvas.width) {
-  snake.x = 0;
-}
-
-// situation where the snake collides with the vertical axises
-
-if (snake.y < 0) {
-  snake.y = canvas.height - grid;
-} else if (snake.y >= canvas.height) {
-  snake.y = 0;
-}
-
-// drawing an apple
-context.fillStyle = 'red';
-context.fillRect(apple.x, apple.y, grid - 1, grid - 1);
-
-// Draw the snake on the canvas
-context.fillStyle = 'green';
-snake.cells.forEach(function (cell, index) {
-  context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
-
-  //   When Snake cell collides Apple
-  if (cell.x == apple.x && cell.y == apple.y) {
-    snake.maxCells++;
-
-    // apple moves another random location
-    apple.x = getRandomInt(0, canvas.width / 16) * grid;
-    apple.y = getRandomInt(0, canvas.height / 16) * grid;
+  // situation where the snake collides with the horizontal axises
+  if (snake.x < 0) {
+    snake.x = canvas.width - grid;
+  } else if (snake.x >= canvas.width) {
+    snake.x = 0;
   }
-});
+
+  // situation where the snake collides with the vertical axises
+
+  if (snake.y < 0) {
+    snake.y = canvas.height - grid;
+  } else if (snake.y >= canvas.height) {
+    snake.y = 0;
+  }
+
+  // snake body movement
+  const snakeBodyMovement = () => {
+    snake.cells.unshift({ x: snake.x, y: snake.y });
+
+    if (snake.cells.length > snake.maxCells) {
+      snake.cells.pop();
+    }
+  };
+  snakeBodyMovement();
+
+  // drawing an apple
+  context.fillStyle = 'red';
+  context.fillRect(apple.x, apple.y, grid - 1, grid - 1);
+
+  // Draw the snake on the canvas
+  context.fillStyle = 'green';
+  snake.cells.forEach(function (cell, index) {
+    context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
+
+    //   When Snake cell collides Apple
+    if (cell.x == apple.x && cell.y == apple.y) {
+      snake.maxCells++;
+
+      // apple moves another random location
+      apple.x = getRandomInt(0, canvas.width / 16) * grid;
+      apple.y = getRandomInt(0, canvas.height / 16) * grid;
+    }
+
+    // modified bubble sort
+    for (let i = index + 1; i < snake.cells.length; i++) {
+      // if one piece of the snakes body is occupying the same space as another piece of the snakes body, reset the game
+      if (cell.x == snake.cells[i].x && cell.y == snake.cells[i].y) {
+        snake.x = 100;
+        snake.y = 100;
+        snake.dx = grid;
+        snake.dy = 0;
+        snake.maxCells = 1;
+        snake.cells = [];
+
+        apple.x = getRandomInt(0, canvas.width / 16) * grid;
+        apple.y = getRandomInt(0, canvas.height / 16) * grid;
+      }
+    }
+  });
+};
