4b91901168a4d7d4e6c99763bb88c425f51057a2
AyakaB <ayaka.bando22@gmail.com>
2023-01-16 13:35:23 -0800

    fixed some bugs

--- snake_lab/feature-state/snake.js
+++ snake_lab/feature-state/snake.js
@@ -1,31 +1,34 @@
+let canvas = document.getElementById('game');
+let context = canvas.getContext('2d');
+
+const grid = 16;
+let count = 0;
+let userSpeed = 6;
+
 let snake = {
-  x: 100,
-  y: 100,
+  x: 160,
+  y: 160,
   dx: grid,
   dy: 0,
   cells: [],
-  maxCells: 1,
+  maxCells: 4,
 };
-let apple = { x: 50, y: 50 };
-const grid = 16;
-let canvas;
-let userSpeed = 6;
-let count = 0;
+let apple = {
+  x: 160,
+  y: 160,
+};
 
-const game = document.getElementById('game');
-let context = game.getContext('2d');
-
-const getRandomInt = (min, max) => {
+function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-};
+}
 
-const loop = () => {
+function loop() {
   requestAnimationFrame(loop);
 
   if (++count < userSpeed) {
     return;
   }
-  count === 0;
+  count = 0;
 
   context.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -60,39 +63,39 @@ const loop = () => {
   snake.cells.forEach(function (cell, index) {
     context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
 
-    if (cell.x == apple.x && cell.y == apple.y) {
+    if (cell.x === apple.x && cell.y === apple.y) {
       snake.maxCells++;
 
-      apple.x = getRandomInt(0, canvas.width / 16) * grid;
-      apple.y = getRandomInt(0, canvas.height / 16) * grid;
+      apple.x = getRandomInt(0, 25) * grid;
+      apple.y = getRandomInt(0, 25) * grid;
     }
 
-    for (let i = index + 1; i < snake.cells.length; i++) {
-      if (cell.x == snake.cells[i].x && cell.y == snake.cells[i].y) {
-        snake.x = 100;
-        snake.y = 100;
+    for (var i = index + 1; i < snake.cells.length; i++) {
+      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+        snake.x = 160;
+        snake.y = 160;
+        snake.cells = [];
+        snake.maxCells = 4;
         snake.dx = grid;
         snake.dy = 0;
-        snake.maxCells = 1;
-        snake.cells = [];
 
-        apple.x = getRandomInt(0, canvas.width / 16) * grid;
-        apple.y = getRandomInt(0, canvas.height / 16) * grid;
+        apple.x = getRandomInt(0, 25) * grid;
+        apple.y = getRandomInt(0, 25) * grid;
       }
     }
   });
-};
+}
 
 document.addEventListener('keydown', function (e) {
   if (e.which === 37 && snake.dx === 0) {
     snake.dx = -grid;
     snake.dy = 0;
-  } else if (e.which === 39 && snake.dx === 0) {
-    snake.dx = grid;
-    snake.dy = 0;
   } else if (e.which === 38 && snake.dy === 0) {
     snake.dy = -grid;
     snake.dx = 0;
+  } else if (e.which === 39 && snake.dx === 0) {
+    snake.dx = grid;
+    snake.dy = 0;
   } else if (e.which === 40 && snake.dy === 0) {
     snake.dy = grid;
     snake.dx = 0;
--- snake_lab/game-state/snake.js
+++ snake_lab/game-state/snake.js
@@ -1,34 +1,29 @@
 let canvas = document.getElementById('game');
-let context = game.getContext('2d');
+let context = canvas.getContext('2d');
 
-// - an immutable variable for the canvas grid, called "grid"
 const grid = 16;
-
-// - mutable variables for snake speed and the count of speed, called "userSpeed" & "count". give count a default value of 0, and userSpeed a default value of 6.
-let userSpeed = 6;
 let count = 0;
+let userSpeed = 6;
 
-// - mutable object variables for both the snake & the apple, called "snake" & "apple"
 let snake = {
-  x: 100,
-  y: 100,
-  // snake velocity. moves one grid length every frame in either the x or y direction
+  x: 160,
+  y: 160,
   dx: grid,
   dy: 0,
-
   // keep track of all grids the snake body occupies
-  // - an empty array to hold the snakes body, called "cells"
   cells: [],
-  // - a default value for initial snake length, called "maxCells", with an initial value of 1
-  maxCells: 1,
+  maxCells: 4,
 };
-let apple = { x: 50, y: 50 };
+let apple = {
+  x: 160,
+  y: 160,
+};
 
-const getRandomInt = (min, max) => {
+function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-};
+}
 
-const loop = () => {
+function loop() {
   // make smooth animation with looping
   requestAnimationFrame(loop);
 
@@ -41,7 +36,7 @@ const loop = () => {
   // to clear the entire canvas
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  // snake movement
+  // snake movement + velocity
   snake.x += snake.dx;
   snake.y += snake.dy;
 
@@ -53,17 +48,22 @@ const loop = () => {
   }
 
   // situation where the snake collides with the vertical axises
-
   if (snake.y < 0) {
     snake.y = canvas.height - grid;
   } else if (snake.y >= canvas.height) {
     snake.y = 0;
   }
 
+  /* snake.cells.unshift({ x: snake.x, y: snake.y });
+
+        if (snake.cells.length > snake.maxCells) {
+          snake.cells.pop();
+        } */
   // snake body movement
   const snakeBodyMovement = () => {
     snake.cells.unshift({ x: snake.x, y: snake.y });
 
+    // remove cells as we move away from them
     if (snake.cells.length > snake.maxCells) {
       snake.cells.pop();
     }
@@ -72,53 +72,53 @@ const loop = () => {
 
   // drawing an apple
   context.fillStyle = 'red';
-  context.fillRect(apple.x, apple.y, grid, grid);
+  context.fillRect(apple.x, apple.y, grid - 1, grid - 1);
 
   // Draw the snake on the canvas
   context.fillStyle = 'green';
   snake.cells.forEach(function (cell, index) {
-    context.fillRect(cell.x, cell.y, grid, grid);
+    context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
 
     //   When Snake cell collides Apple
-    if (cell.x == apple.x && cell.y == apple.y) {
+    if (cell.x === apple.x && cell.y === apple.y) {
       snake.maxCells++;
 
       // apple moves another random location
-      apple.x = getRandomInt(0, canvas.width / 16) * grid;
-      apple.y = getRandomInt(0, canvas.height / 16) * grid;
+      apple.x = getRandomInt(0, 25) * grid;
+      apple.y = getRandomInt(0, 25) * grid;
     }
 
     // modified bubble sort
-    for (let i = index + 1; i < snake.cells.length; i++) {
+    for (var i = index + 1; i < snake.cells.length; i++) {
       // if one piece of the snakes body is occupying the same space as another piece of the snakes body, reset the game
-      if (cell.x == snake.cells[i].x && cell.y == snake.cells[i].y) {
-        snake.x = 100;
-        snake.y = 100;
+      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+        snake.x = 160;
+        snake.y = 160;
+        snake.cells = [];
+        snake.maxCells = 4;
         snake.dx = grid;
         snake.dy = 0;
-        snake.maxCells = 1;
-        snake.cells = [];
 
-        apple.x = getRandomInt(0, canvas.width / 16) * grid;
-        apple.y = getRandomInt(0, canvas.height / 16) * grid;
+        apple.x = getRandomInt(0, 25) * grid;
+        apple.y = getRandomInt(0, 25) * grid;
       }
     }
   });
-};
+}
 
 document.addEventListener('keydown', function (e) {
   if (e.which === 37 && snake.dx === 0) {
     // arrow left
     snake.dx = -grid;
     snake.dy = 0;
-  } else if (e.which === 39 && snake.dx === 0) {
-    // arrow right
-    snake.dx = grid;
-    snake.dy = 0;
   } else if (e.which === 38 && snake.dy === 0) {
-    // arrow up
+    // arrow right
     snake.dy = -grid;
     snake.dx = 0;
+  } else if (e.which === 39 && snake.dx === 0) {
+    // arrow up
+    snake.dx = grid;
+    snake.dy = 0;
   } else if (e.which === 40 && snake.dy === 0) {
     // arrow down
     snake.dy = grid;
